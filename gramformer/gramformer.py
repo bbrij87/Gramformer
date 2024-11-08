6a0b169ecc36d321270793a3910ebafef0105b10
bbrij87 <bbrij@w3identity.net>
2024-11-08 12:13:02 +1100

    Update gramformer.py

--- gramformer/gramformer.py
+++ gramformer/gramformer.py
@@ -1,6 +1,6 @@
 class Gramformer:
 
-  def __init__(self, models=1, use_gpu=False):
+  def __init__(self, models=1, use_gpu=False, model_path=None):
     from transformers import AutoTokenizer
     from transformers import AutoModelForSeq2SeqLM
     #from lm_scorer.models.auto import AutoLMScorer as LMScorer
@@ -14,7 +14,8 @@ class Gramformer:
     batch_size = 1    
     #self.scorer = LMScorer.from_pretrained("gpt2", device=device, batch_size=batch_size)    
     self.device    = device
-    correction_model_tag = "prithivida/grammar_error_correcter_v1"
+    #correction_model_tag = "prithivida/grammar_error_correcter_v1"
+    correction_model_tag = model_path
     self.model_loaded = False
 
     if models == 1:
